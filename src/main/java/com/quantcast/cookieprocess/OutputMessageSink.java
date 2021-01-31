6120ddf3950ae2c2cb170d630622fac41625d63b
Dragos Cernahoschi <dragos.cernahoschi@sicpa.com>
2021-01-31 16:33:07 +0100

    javadoc

--- src/main/java/com/quantcast/cookieprocess/OutputMessageSink.java
+++ src/main/java/com/quantcast/cookieprocess/OutputMessageSink.java
@@ -1,5 +1,8 @@
 package com.quantcast.cookieprocess;
 
+/**
+ * Interface that abstracts where an application output message is written
+ */
 public interface OutputMessageSink {
   void write(String message);
 }
